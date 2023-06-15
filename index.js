aa7035f1fb3438efd64de99d8ee43390c794a275
Adebisola <adeniyiadebisola@gmail.com>
2023-06-15 16:25:07 +0100

    remove unwanted code

--- index.js
+++ index.js
@@ -46,17 +46,6 @@ let gameObj = {}
 const localStorageGameKey = "HTA"
 
 
-/* <div class="card">
-<div class="card-inner">
-    <div class="card-front">
-        <img src="/images/card-JackClubs.png" alt="" class="card-img">
-    </div>
-    <div class="card-back">
-        <img src="/images/card-back-Blue.png" alt="" class="card-img">
-    </div>
-</div>
-</div> */
-
 
 loadGame()
 
